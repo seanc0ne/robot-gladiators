acd82400bce5ab96e076f1acb00fc1c0aa99f627
Sean Cone <seandoesitbetter@gmail.com>
2020-06-03 19:31:20 -0700

    Whew

--- assets/js/game.js
+++ assets/js/game.js
@@ -56,33 +56,35 @@ var fight = function(enemyName) {
       window.alert(playerName + " has decided to skip this fight. Goodbye!");
       // subtract money from playerMoney for skipping
       playerMoney = playerMoney - 2;
+      // player quits
+      return
     }
   // if no (false), ask question again by running fight() again
  // else {
   //  fight();
  //}
 }
-  //Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
-//   enemyHealth = enemyHealth - playerAttack;
-//   // Log a resulting message to the console so we know that it worked.
-//   console.log(playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining.");
-//   if (enemyHealth <= 0) {
-//     window.alert(enemyName + " has died!");
-//   } 
-//   else {
-//     window.alert(enemyName + " still has " + enemyHealth + " health left.");
-//   }
-//   // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
-//   playerHealth = playerHealth - enemyAttack;
-//   // Log a resulting message to the console so we know that it worked.
-//   console.log(enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining.");
-//   if (playerHealth <= 0) {
-//     window.alert(playerName + " has died!");
-//   } 
-//   else {
-//     window.alert(playerName + " still has " + playerHealth + " health left.");
-//   }
-// };
+  // Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
+  enemyHealth = enemyHealth - playerAttack;
+  // Log a resulting message to the console so we know that it worked.
+  console.log(playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining.");
+  if (enemyHealth <= 0) {
+    window.alert(enemyName + " has died!");
+  } 
+  else {
+    window.alert(enemyName + " still has " + enemyHealth + " health left.");
+  }
+  // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
+  playerHealth = playerHealth - enemyAttack;
+  // Log a resulting message to the console so we know that it worked.
+  console.log(enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining.");
+  if (playerHealth <= 0) {
+    window.alert(playerName + " has died!");
+  } 
+  else {
+    window.alert(playerName + " still has " + playerHealth + " health left.");
+  }
+};
 
 for (var i = 0; i < enemyNames.length; i++) {
   var pickedEnemyName = enemyNames[i];
